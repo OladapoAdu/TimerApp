7ef8817263bad60eee0a3025bbb266e012a888c6
Destiny Oladapo Adu <aduoladapo9@gmail.com>
2025-09-25 12:42:22 +0100

    Settings screen.

---
+++ lib/settings.dart
@@ -0,0 +1,251 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Settings'),
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Colors.grey,
+        
+        title: Text(widget.title),
+      ),
+      body: Center(
+        
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Text("Work",
+            style: TextStyle(fontSize: 20),textAlign: TextAlign.left,),
+
+            SizedBox(height: 20,),
+
+            Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                    });
+                  },
+                  child: Text("-",
+                  style: TextStyle(
+                    fontSize: 20
+                  ),),
+                ),
+              ),
+              SizedBox(width: 5),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+
+                    });
+                  },
+                  child: Text(""),
+                ),
+              ),
+              SizedBox(width: 5),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                    });
+                  },
+                  child: Text("+",
+                  style: TextStyle(fontSize: 20),),
+                ),
+              ),
+            ],
+          ),
+
+          Text("Short",
+            style: TextStyle(fontSize: 20),textAlign: TextAlign.left,),
+
+            SizedBox(height: 20,),
+
+            Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                    });
+                  },
+                  child: Text("-",
+                  style: TextStyle(
+                    fontSize: 20
+                  ),),
+                ),
+              ),
+              SizedBox(width: 5),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+
+                    });
+                  },
+                  child: Text(""),
+                ),
+              ),
+              SizedBox(width: 5),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                    });
+                  },
+                  child: Text("+",
+                  style: TextStyle(fontSize: 20),),
+                ),
+              ),
+            ],
+          ),
+
+          Text("Long",
+            style: TextStyle(fontSize: 20),textAlign: TextAlign.left,),
+
+            SizedBox(height: 20,),
+
+            Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                    });
+                  },
+                  child: Text("-",
+                  style: TextStyle(
+                    fontSize: 20
+                  ),),
+                ),
+              ),
+              SizedBox(width: 5),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+
+                    });
+                  },
+                  child: Text(""),
+                ),
+              ),
+              SizedBox(width: 5),
+              Expanded(
+                child: ElevatedButton(style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                    });
+                  },
+                  child: Text("+",
+                  style: TextStyle(fontSize: 20),),
+                ),
+              ),
+            ],
+          ),
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
+
+
